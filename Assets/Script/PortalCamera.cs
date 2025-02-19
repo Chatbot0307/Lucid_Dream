6899a505e87a1bb518a01e069843fff3346264ac
Chatbot0307 <148403963+Chatbot0307@users.noreply.github.com>
2025-02-20 02:30:11 +0900

    Portal system
    포탈 시스템 구현 및 그래픽 수정

---
+++ Assets/Script/PortalCamera.cs
@@ -0,0 +1,21 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    void LateUpdate()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+}
