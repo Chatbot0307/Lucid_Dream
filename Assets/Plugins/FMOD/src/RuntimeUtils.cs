8158f5b8fb6a0d6c50f0da0dbc0bba2f68c4dd1e
Chatbot0307 <148403963+Chatbot0307@users.noreply.github.com>
2025-03-01 00:54:21 +0900

    Finally FMOD
    FMOD이제 진짜 된다

--- Assets/Plugins/FMOD/src/RuntimeUtils.cs
+++ Assets/Plugins/FMOD/src/RuntimeUtils.cs
@@ -607,7 +607,6 @@ namespace FMODUnity
         {
             if (!Settings.IsInitialized() || Settings.Instance.LoggingLevel >= FMOD.DEBUG_FLAGS.ERROR)
             {
-                Debug.LogException(e);
             }
         }
 
--- Assets/Script/ButtonUI.cs
+++ Assets/Script/ButtonUI.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+using FMODUnity;
+using FMOD.Studio;
 public class ButtonUI : MonoBehaviour
 {
     public RawImage[] images;
     public Color selectedColor;
     public Color PressColor;
     public Color normalColor;
+    public EventReference source;
+    public EventInstance audioInstance;
+
+    private void Start()
+    {
+        audioInstance = RuntimeManager.CreateInstance(source);
+        audioInstance.start();
+    }
 
     public void SelectButton(int val)
     {
